751445bada50f97064b3e3ece97d97a5c9c7e06c
tdelpidio <tdelpidio@gmail.com>
2022-07-15 01:54:25 -0400

    sizing adjusted

---
+++ index.js
@@ -0,0 +1,6 @@
+const resizeOps = () => {
+    document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+  };
+
+  resizeOps();
+  window.addEventListener("resize", resizeOps);
